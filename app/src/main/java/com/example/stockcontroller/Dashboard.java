7a73f844455e772571e587ed5bc03fc252e52756
Reinharrd <reinhaardsagala10@gmail.com>
2023-06-19 22:00:20 +0700

    revisi recyclerview
    revisi recyclerview

--- app/src/main/java/com/example/stockcontroller/Dashboard.java
+++ app/src/main/java/com/example/stockcontroller/Dashboard.java
@@ -8,13 +8,18 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-//import com.google.firebase.database.DatabaseReference;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -24,10 +29,10 @@ public class Dashboard extends AppCompatActivity implements View.OnClickListener
     private TextView tvNamaGudang, tvItemCount;
     private ImageView imgProfile, imgSort;
 
-//    private RecyclerView recyclerView;
-//    private ArrayList<ModelBarang> list;
-//    private DatabaseReference dbref;
-//    private AdapterBarang adapter;
+    private RecyclerView recyclerView;
+    private ArrayList<ModelBarang> list;
+    private DatabaseReference dbref;
+    private AdapterBarang adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,28 +51,29 @@ public class Dashboard extends AppCompatActivity implements View.OnClickListener
         imgProfile.setOnClickListener(this);
         imgSort.setOnClickListener(this);
 
-//        recyclerView = findViewById(R.id.rv_dashboard_barang);
-//        dbref = FirebaseDatabase.getInstance().getReference().child("DataBarang");
-//        list = new ArrayList<>();
-//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-//        adapter = new AdapterBarang(this,list);
-//        recyclerView.setAdapter(adapter);
-//
-//        dbref.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                for (DataSnapshot dataSnapshot: snapshot.getChildren()){
-//                    ModelBarang modelBarang = dataSnapshot.getValue(ModelBarang.class);
-//                    list.add(modelBarang);
-//                }
-//                adapter.notifyDataSetChanged();
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//
-//            }
-//        });
+        recyclerView = findViewById(R.id.rv_dashboard_barang);
+        dbref = FirebaseDatabase.getInstance().getReference().child("DataBarang");
+        list = new ArrayList<>();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new AdapterBarang(this,list);
+        recyclerView.setAdapter(adapter);
+
+        dbref.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    ModelBarang modelBarang = dataSnapshot.getValue(ModelBarang.class);
+                    list.add(modelBarang);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 
